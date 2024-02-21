b948f831c43ec26ee42fb90d58e9581b264b0d8c
nadupoy <gnsampao@gmail.com>
2024-02-21 17:31:29 +0300

    Added an attribution component. Refactored the code that renders data.json.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import DATA from "./data.json";
 
 function Result() {
   return (
-    <div>
+    <div className="score-section">
       <p>Your Result</p>
 
-      <section>
+      <section className="score">
         <p>
-          <span>76</span>
+          <span className="total-score">76</span>
           <br />
           of 100
         </p>
@@ -16,7 +16,7 @@ function Result() {
 
       <h2>Great</h2>
 
-      <p>
+      <p className="score-statement">
         You scored higher than 65% of the people who have taken these tests.
       </p>
     </div>
@@ -25,20 +25,26 @@ function Result() {
 
 function SummaryData() {
   return (
-    <div>
-      {DATA && DATA.map((data) => {
-        return (
-          <div key={data.id}>
-            <img src={data.icon} key={data.key} />
+    <div className="summary-section">
+      <section className="summary-contents">
+        <h2>Summary</h2>
 
-            <p>{data.category}</p>
+        <ul>
+          {DATA.map((data) => {
+            return (
+              <li key={data.id}>
+                <img src={data.icon} />
 
-            <section>
-              <span>{data.score}</span> / 100
-            </section>
-          </div>
-        );
-      })}
+                <p className="category">{data.category}</p>
+
+                <p className="category-score">
+                  <span>{data.score}</span> / 100
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
@@ -47,9 +53,19 @@ function Button() {
   return <button>Continue</button>;
 }
 
+function Attribution() {
+  return (
+    <div className="attribution">
+      <p>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="https://github.com/nadupoy" target="_blank">Nadupoy</a>.
+      </p>
+    </div>
+  );
+}
+
 function Summary() {
   return (
-    <div>
+    <div className="summary-section">
       <SummaryData />
 
       <Button />
@@ -59,10 +75,12 @@ function Summary() {
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Result />
 
       <Summary />
+
+      <Attribution />
     </div>
   );
 }
